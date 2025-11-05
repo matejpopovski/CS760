742fd1519ed6734fab6aaa7bd6395368ae045ced
matejpopovski <matej.popovski@gmail.com>
2025-11-04 20:14:14 -0600

    hw3

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -208,3 +208,4 @@ print(f"[ReLU] Test accuracy: {test_acc_relu:.2f}%")
 
 print("\n=> ReLU accelerates convergence and yields higher accuracy by avoiding gradient saturation seen with sigmoid activations.")
 
+
